0d5a0f02d2346a88adebdc5c7f76fc8670c846fd
SONG MINGLUN <soumeirin123@gmail.com>
2021-04-24 18:34:23 +0900

    add some thing

---
+++ camera.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+
+    ret, frame = cap.read()
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
