77424934552846cf640c1562cb892d0fcabae647
SONG MINGLUN <soumeirin123@gmail.com>
2021-04-28 16:48:03 +0900

    add a dir

---
+++ 0_ConpisiteVideo/frame2video.py
@@ -0,0 +1,29 @@
+import cv2
+import glob
+from tqdm import tqdm
+import os
+
+def makeVideo(path, size):
+    filelist = os.listdir(path)
+    filelist2 = [os.path.join(path, i) for i in filelist]
+    filelist2 = sorted(filelist2)
+    print(filelist2)
+    fps = 30  
+    video = cv2.VideoWriter(path + "/Video.mp4", cv2.VideoWriter_fourcc('M', 'P', '4', 'v'), fps,
+                            size) 
+
+    for item in tqdm(filelist2):
+        # print(item)
+        if item.endswith('.jpg'):
+            # print(item)
+            img = cv2.imread(item)
+            video.write(img)
+
+    video.release()
+    cv2.destroyAllWindows()
+    print('DONE')
+
+        
+
+if __name__ == '__main__':
+    makeVideo('lab_Introduction', (1280, 720))
