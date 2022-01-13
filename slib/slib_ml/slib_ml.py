ecff852a29ba56ddba1e0b3687325781e3764311
SONG MINGLUN <soumeirin123@gmail.com>
2022-01-13 17:23:45 +0900

    update

--- slib/slib_ml/slib_ml.py
+++ slib/slib_ml/slib_ml.py
@@ -17,3 +17,23 @@ def confusion_matrix(list_labels, list_predictions):
 
 def print_training_info(train_accuracy, train_loss, test_accuracy, test_loss):
     print('train_accuracy: %.05f train_loss: %.05f test_accuracy: %.05f test_loss: %.05f' % (train_accuracy, train_loss, test_accuracy, test_loss))
+
+def print_graph_info(train_accuracy_list, train_loss_list, test_accuracy_list, test_loss_list):
+
+    plt.plot(range(len(train_loss_list)), train_loss_list, label='list_train_loss')
+    plt.plot(range(len(test_loss_list)), test_loss_list, label='list_test_loss')
+    plt.grid(True)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(bbox_to_anchor=(1,1), loc='center left')
+    plt.show()
+
+    plt.plot(range(len(train_accuracy_list)), train_accuracy_list, label='train_accuracy')
+    plt.plot(range(len(test_accuracy_list)), test_accuracy_list, label='test_accuracy')
+    plt.grid(True)
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(bbox_to_anchor=(1,1), loc='center left')
+    plt.show()
+
+
